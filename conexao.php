c64a821d57165e135c15256420eced4e5b763862
erisjunior <erisvan.junior.a@gmail.com>
2017-11-13 07:46:07 -0300

    Menu Top

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST", "localhost");
+define("USER", "root");
+define("PASS", "");
+define("DB", "loja");
+$con = mysqli_connect(HOST,USER,PASS) or die(" when i outside and saw the moom,i breathe and think of you, i'm think of you ooo ");
+$banco = mysqli_select_db($con, DB);
+?>
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ZTekCorp</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/slate/bootstrap.min.css" rel="stylesheet" integrity="sha384-RpX8okQqCyUNG7PlOYNybyJXYTtGQH+7rIKiVvg1DLg6jahLEk47VvpUyS+E2/uJ" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+
+<div class="row" id="rowtop">
+	<div class="top">
+		<div class="col-sm-3">
+			<img class="logo" src="Logo/LogoFi.png">
+		</div>
+
+		<div class="col-sm-2">
+			<div class="form-group">
+		      <select id="selec_cat" class="form-control">
+		        <option>Categoria...</option>
+		        <option value="acessorios">Acessorios</option>
+		        <option value="gadgets">Gadgets</option>
+		        <option value="smartphones">Smartphones</option>
+		        <option value="gaming">Gaming</option>
+		        <option value="computadores">Computadores</option>
+		      </select>
+		    </div>
+		</div>
+
+		<div class="col-sm-3">
+		    <div class="input-group">
+	            <input type="text" class="form-control" placeholder="Pesquisa...">
+	            <div class="input-group-addon">
+	            	<a><span class="glyphicon glyphicon-search"></span></a>
+	            </div>
+	        </div>
+	    </div>
+
+		<div class="col-sm-1"></div>
+
+		<div class="col-sm-3">
+			<span style="font-size: 34px" class="glyphicon glyphicon-user"></span>  Minha Conta
+
+			<span style="margin-left: 20px;font-size: 34px" class="glyphicon glyphicon-shopping-cart"></span>Carrinho
+
+		</div>
+	</div>
+</div>
+</body>
+</html>
