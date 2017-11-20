8b08b1023a162a699ac03a6afda7e4bf7fc85b64
MacielGustavo323 <33124237+MacielGustavo323@users.noreply.github.com>
2017-11-20 08:06:54 -0300

    Add files via upload
    Footer (por terminar)

--- footer.php
+++ footer.php
@@ -1,17 +1,80 @@
+<style type="text/css">
+	#titulo{
+		font-size: 18px;
+		font-family: FontAwesome;
+	}
+	#conteudo{
+		font-size: 14px;
+		font-family: FontAwesome;
+		text-align: left;
+	}
+
+
+</style>
+
 <footer id="footer">
-	<div class="row" style="background: url(img/vialactea.png);">
+<div class="row" style="background: url(img/vialactea.png);">
 
-		<div class="col-sm-12">
-
-		</div>
+	<div class="col-sm-12">
 
 	</div>
 
-	<div class="row" style="background: #222;padding: 10px">
+</div>
+<div class="row">
+	<div class="col-sm-12" style="background: url(img/vialactea.png);">		
+		<div class="col-sm-4" >
+		<label id="titulo">Sobre Nós:</label>
 
-		<div class="col-sm-12">
-			&copy; Copyright Reserved to ZTekCorp Corp
+		<label id="conteudo">Ztekcorp fundada em 1930, a empresa tem como principal intuito
+		fornecer as mais variadas tecnologias ao mercado. Um dos pontos fortes da mesma é
+		a criação de novas tecnologias que tendem a inovar  e facilitar a vida das pessoas
+		que as utilizam. A Ztekcorp tem uma grande rede de lojas e empresas associadas a mesma
+		que ajudou a empresa a crescer  no mercado nacional e internacional de criação e vendas 
+		de novas tecnologias, mas existe uma informaçao que poucos sabem, que é sobre seus criadores,
+		que são estudantes de uma escola profissionalisante e que são grandes gênios que nasceram para revolucionar,
+		inovar e superar tudo já criado por outros gênios. 
+
+		</label>
 		</div>
 
+		<div class="col-sm-4">
+
+			<label id="titulo">Informações Adicionais</label>
+
+			<form class="button-gruop"> 
+				<ul>
+					<input class="btn btn-primary" type="submit" value="youtube">
+					<input class="btn btn-primary" type="submit" value="facebook">
+					<input class="btn btn-primary" type="submit" value="twitter">
+					<input class="btn btn-primary" type="submit" value="Email">
+					<input class="btn btn-primary" type="submit" value="telefone">
+					<input class="btn btn-primary" type="submit" value="Endereço">
+				</ul>
+	</form>
+</div>
+
+	<div class="col-sm-4" >
+		
+			<label id="titulo">Comentarios:</label>
+		
+		<form>
+			<input type="email" class="form-control" placeholder="Email">
+			<input type="text"  class="form-control" placeholder="Comentario" style="height: 70px" >
+			<input class="btn btn-primary" type="submit" value="Enviar">
+		</form>
+
 	</div>
+
+</div>
+
+
+</div>
+<div class="row" style="background: #222;padding: 10px">
+
+	<div class="col-sm-12">
+		&copy; Copyright Reserved to ZTekCorp Corp
+	</div>
+
+</div>
+
 </footer>
