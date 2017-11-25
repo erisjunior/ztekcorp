5ce5753e932102ce24f474263fc0db3f9eb727e5
Eronheit <32879462+Eronheit@users.noreply.github.com>
2017-11-25 08:43:53 -0200

    Add files via upload
    Ztekcorp sem bugs + banco (tinha esquecido)

--- loja.sql
+++ loja.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 18-Nov-2017 às 12:29
--- Versão do servidor: 10.1.21-MariaDB
--- PHP Version: 7.0.15
+-- Generation Time: 25-Nov-2017 às 11:40
+-- Versão do servidor: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -40,6 +42,30 @@ CREATE TABLE `admin` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `msg` text NOT NULL,
+  `prod_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `nome`, `email`, `msg`, `prod_id`) VALUES
+(1, 'vkjnk@rnkjnf', 'gg@mn', 'Muto bom', 23),
+(2, '', '', '', 23),
+(3, '', '', '', 23),
+(4, '', '', '', 23);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `produto`
 --
 
@@ -57,14 +83,11 @@ CREATE TABLE `produto` (
 --
 
 INSERT INTO `produto` (`id`, `nome`, `image`, `preco`, `categoria`, `view`) VALUES
-(1, 'PC GAMER MOBA BOX II - I5 + GTX 1050 TI 4GB', 'pc1.png', '3300.00', 'Gaming', 4),
-(2, 'PcGamer I5 Placa De Video GTX-1080 TI 8GB RAM-DDR4', 'pc3.jpg', '3650.00', 'Gaming', 6),
-(3, 'Kit Gamer(PC-Keyborad-Mouse-HeadSet-Monitor-MousePad)', 'pc2.jpg', '5000.00', 'Gaming', 15),
-(4, 'PC GAMER MOBA BOX II - I5 + GTX 1050 TI 4GB', 'pc1.png', '3300.00', 'Gaming', 4),
-(16, 'PC GAMER MOBA BOX II - I5 + GTX 1050 TI 4GB', 'pc1.png', '3300.00', 'Gaming', 4),
-(17, 'PcGamer I5 Placa De Video GTX-1080 TI 8GB RAM-DDR4', 'pc3.jpg', '3650.00', 'Gaming', 6),
-(18, 'Kit Gamer(PC-Keyborad-Mouse-HeadSet-Monitor-MousePad)', 'pc2.jpg', '5000.00', 'Gaming', 15),
-(19, 'PC GAMER MOBA BOX II - I5 + GTX 1050 TI 4GB', 'pc1.png', '3300.00', 'Gaming', 4);
+(22, 'aviao', 'c8ed49ceaaf4108a690988b6e032fdc9.jpg', '8700000.00', 'Gadgets', 114),
+(23, 'Notbook', '54a082d61bf62741f7293f9559a0eb82.jpg', '2000.00', 'Computador', 125),
+(24, 'Celular de junior', '37157d89e87c57d6ecac2dc59583e3c4.jpg', '1000.00', 'SmartPhone', 136),
+(25, 'placa de video', '6cc740c4f07962f2d6c574a70afd3911.jpg', '10000.00', 'Gaming', 57),
+(26, 'nada', '8430cd788d8ae50c17461c1e83edf449.jpg', '1000.00', 'Acessorio', 0);
 
 -- --------------------------------------------------------
 
@@ -90,7 +113,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `cpf`, `nasc`, `end`, `tel`, `email`, `user`, `senha`, `image`) VALUES
-(8, 'Josimar Martins', '413.624.753-75', '17/10/2000', 'Coronel, 94', '(84) 9.8161-3081', 'josimarjr479@gmail.com', 'jrjosimar04', '42e86ad8c7f643c002b5ecf66dd67b1a', '8f82a02697ed4ed6398940f09c21de6e.jpg');
+(8, 'Josimar Martins', '413.624.753-75', '17/10/2000', 'Coronel, 94', '(84) 9.8161-3081', 'josimarjr479@gmail.com', 'jrjosimar04', '42e86ad8c7f643c002b5ecf66dd67b1a', '8f82a02697ed4ed6398940f09c21de6e.jpg'),
+(9, 'Eronheit', '121.563.213-72', '21/89/7301', 'yasdghakshbdaasbdkvsbdksaghdvsadsavdghasdvhdsad', '(88) 9.9718-7576', 'eronheit@heit.com', 'Eronheit', '774355d0d99fd6c47b38c2097471b409', 'db6a99515ad4aad2f448958abb2ac616.jpg');
 
 --
 -- Indexes for dumped tables
@@ -100,6 +124,12 @@ INSERT INTO `usuario` (`id`, `nome`, `cpf`, `nasc`, `end`, `tel`, `email`, `user
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comentarios`
+--
+ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -123,16 +153,26 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
